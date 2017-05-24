e243835cac63c43c22ae41253d2458dbe4ade4d1
nacer <nacer15oct@gmail.com>
2017-05-24 15:43:07 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,60 @@
 import { Component } from '@angular/core';
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+import { AngularFireAuth } from 'angularfire2/auth';
+import * as firebase from 'firebase/app';
+import { Observable } from 'rxjs/Observable';
+
+class Message{
+  uid: string;
+  body: string;
+  id: string;
+
+  constructor(){}
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app works!';
+  user: Observable<firebase.User>;
+  items: FirebaseListObservable<any[]>;
+  message: Message = new Message();
+  data : any;
+  user_displayName: string;
+  user_avatar;
+  user_mail;
+  constructor(public afAuth: AngularFireAuth, public af: AngularFireDatabase){
+    this.user = afAuth.authState;
+    this.user.subscribe(user => {
+        if (!user) {
+          this.user_displayName = null;        
+          return;
+        }
+        this.user_displayName = user.displayName;
+        this.user_avatar = user.photoURL;
+        this.user_mail = user.email;
+      }
+    );
+    /*
+    // push data to database
+    //let data = this.af.list('/messages').push(this.message);
+
+
+    // get data
+    //this.items = af.list('/messages');
+    */
+
+  }
+  login(){
+    this.afAuth.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider()).then(res => console.log(res));
+  }
+  signOut() {
+    this.afAuth.auth.signOut();
+    this.user_avatar = null;
+    this.user_displayName = null;
+    this.user_mail = null;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,20 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+
+export const config = {
+    apiKey: "AIzaSyC6TnmBIjEhLQaAdwiG8J5mjN_T8ZZOsiI",
+    authDomain: "testfire-f205e.firebaseapp.com",
+    databaseURL: "https://testfire-f205e.firebaseio.com",
+    projectId: "testfire-f205e",
+    storageBucket: "testfire-f205e.appspot.com",
+    messagingSenderId: "232626622300"
+  };
+
+
 @NgModule({
   declarations: [
     AppComponent
@@ -12,7 +26,10 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AngularFireModule.initializeApp(config),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
